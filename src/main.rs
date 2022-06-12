d2cd5e1fb9fc462a56853401973ddf80b35f58a9
Arne <arne.carla.jacobs@outlook.com>
2022-06-12 10:32:44 +0200

    made a boid

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use bevy_inspector_egui::WorldInspectorPlugin;
 use smooth_bevy_cameras::{controllers::fps::{FpsCameraBundle, FpsCameraController, FpsCameraPlugin}, LookTransformPlugin};
 use bevy::render::primitives::Aabb;
 use bevy_prototype_debug_lines::*;
+use bevy::render::mesh::{self, PrimitiveTopology};
+use std::f32::consts::PI;
 
 struct Field(Aabb);
 
@@ -22,6 +24,7 @@ fn main() {
         })
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup_graphics)
+        .add_startup_system(setup)
         .add_system(cursor_grab_system)
         .add_system(draw_field_system)
         .insert_resource(Field(Aabb::from_min_max(
@@ -135,4 +138,68 @@ pub fn draw_bounding_box(lines: &mut ResMut<DebugLines>, aabb: &Aabb) {
             Color::GREEN
         );
     }
+}
+
+fn create_bpr_bundle(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) -> PbrBundle {
+
+    let vertices = [
+        ([0.0, 2.0, 0.0], [1.0, 1.0]),
+        ([((2.0 * PI) * 0.0/3.0).sin(), 0.0, ((2.0 * PI) * 0.0/3.0).cos()], [1.0, 1.0]),
+        ([((2.0 * PI) * 1.0/3.0).sin(), 0.0, ((2.0 * PI) * 1.0/3.0).cos()], [1.0, 1.0]),
+        ([((2.0 * PI) * 2.0/3.0).sin(), 0.0, ((2.0 * PI) * 2.0/3.0).cos()], [1.0, 1.0]),
+    ];
+
+    let indices = mesh::Indices::U32(
+        vec![
+        0, 1, 2,
+        0, 2, 3,
+        0, 3, 1,
+        1, 3, 2,
+        ]);
+
+    let mut positions = Vec::new();
+    //let mut normals = Vec::new();
+    let mut uvs = Vec::new();
+    for (position,uv) in vertices.iter() {
+        positions.push(*position);
+        //normals.push(*normal);
+        uvs.push(*uv);
+    }
+
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.set_indices(Some(indices));
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    //mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+    mesh.duplicate_vertices();
+    mesh.compute_flat_normals();
+
+    // add entities to the world
+    return PbrBundle {
+        mesh: meshes.add(mesh),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    };
+
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    ) {
+    commands.spawn_bundle(create_bpr_bundle(meshes, materials));
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
 }
